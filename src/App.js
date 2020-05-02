86e491ccfc4a7739bb19effcc8ed700e0d36f32c
Fangyuan Zhang <zhan535@usc.edu>
2020-05-02 12:30:34 -0700

    correct typo on the page

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi , I'am a Unit Converter~ (ฅ´ω`ฅ) </h1>
+        <h1>Hi , I'm a Unit Converter~ (ฅ´ω`ฅ) </h1>
         <hr></hr>
         <div className="Body">
           <InputNum 
